a235fe058c8fa733fbae2798002bb5b996766c22
iskanderaues01 <Synyni1111@mail.ru>
2025-03-31 00:44:21 +0500

    fix 1

--- services/paths_service.py
+++ services/paths_service.py
@@ -5,7 +5,7 @@ def get_ml_results_path() -> str:
     Возвращает путь к директории для сохранения результатов ML.
     При необходимости здесь можно сменить стандартный путь.
     """
-    ml_dir = "/home/chaplin/Desktop/ml_results"
+    ml_dir = "Z:\\project_car_metrics\\ml_results"
     os.makedirs(ml_dir, exist_ok=True)
     return ml_dir
 
@@ -13,7 +13,7 @@ def get_parsed_data_path() -> str:
     """
     Возвращает путь к директории для сохранения результатов парсинга (CSV).
     """
-    parsed_dir = "/home/chaplin/Desktop/parsed_data"
+    parsed_dir = "Z:\\project_car_metrics\\parsed_data\\"
     os.makedirs(parsed_dir, exist_ok=True)
     return parsed_dir
 
